cf7edb7f26a71e7c50b0db12adfc5fd1d7571fa8
darshan377 <darshan377>
2026-01-09 22:10:23 +0600

    product-details page updated

--- frontend/script/approve-post.js
+++ frontend/script/approve-post.js
@@ -84,7 +84,7 @@ async function rejectProduct() {
 }
 
  function loadFooter() {
-    fetch("http://127.0.0.1:5500/frontend/footer.html")
+    fetch("http://127.0.0.1:5500/footer.html")
       .then(res => res.text())
       .then(html => {
         document.getElementById("footer").innerHTML = html;
